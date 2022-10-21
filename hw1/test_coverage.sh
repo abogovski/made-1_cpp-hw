962641824a2e962513b3a8cbb1cd7820e53ee8cf
abogovski <abogovski@gmail.com>
2022-10-21 20:11:57 +0300

    Don't forget to make test_coverage dir

--- hw1/test_coverage.sh
+++ hw1/test_coverage.sh
@@ -2,5 +2,6 @@ cmake -DENABLE_COVERAGE=1 . && make all
 
 lcov -z --directory .
 make test
+mkdir test_coverage
 lcov -c --directory . --output-file test_coverage/coverage.info
 genhtml test_coverage/coverage.info --output-directory test_coverage/
