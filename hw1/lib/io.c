6ac7137ce2f8ffa195a533a935110985e41e8503
abogovski <abogovski@gmail.com>
2022-11-12 00:53:57 +0300

    Try fix actions

--- hw1/lib/io.c
+++ hw1/lib/io.c
@@ -2,41 +2,43 @@
 #include <string.h>
 #include <ctype.h>
 
-#define STRINGIZE_NX(A) #A
-#define _S(A) STRINGIZE_NX(A)
-
-static void consumeTail(FILE* file) {
+static bool readWord(FILE* file, char* word, size_t maxLen) {
     int c = fgetc(file);
-    while (!isspace(c)) {
-        if (c == EOF) {
-            return;
-        }
+    while (c != EOF && isspace(c)) {
         c = fgetc(file);
     }
-    ungetc(c, file);
+    if (c == EOF) {
+        word[0] = '\0';
+        return false;
+    }
+
+    size_t offset = 0;
+    while (c != EOF && !isspace(c) && offset < maxLen) {
+        word[offset++] = c;
+        c = fgetc(file);
+    }
+    word[offset] = '\0';
+
+    while (c != EOF && !isspace(c)) {
+        c = fgetc(file);
+    }
+    return true;
 }
 
 static Body readBody(FILE* file) {
     char body[MAX_BODY_STR_LEN + 1];
-    int rv = fscanf_s(file, " %s", body, MAX_BODY_STR_LEN);
-    if (rv != 1) {
+    if (!readWord(file, body, MAX_BODY_STR_LEN)) {
         return Undefined;
     }
-    consumeTail(file);
     return bodyFromString(body);
 }
 
 bool readModel(FILE* file, char* model) {
-    int rv = fscanf_s(file, " %s", model, MAX_MODEL_STR_LEN);
-    if (rv != 1) {
-        return false;;
-    }
-    consumeTail(file);
-    return true;
+    return readWord(file, model, MAX_MODEL_STR_LEN);
 }
 
 bool readCar(FILE* file, Car* car) {
-    int rv = fscanf_s(
+    int rv = fscanf(
         file,
         "%lf%lf%lf",
         &car->enginePower,
